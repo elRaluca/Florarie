38eba5907e44c50f58a8c4815f8b8d0458b7773d
Gheorghita Raluca <elenaralucalorena@gmail.com>
2024-07-10 14:47:56 +0300

    Updates

---
+++ precache-manifest.9c6c825440eef5c511be5aac4115eff8.js
@@ -0,0 +1,122 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a597068e46bbcaeb653ebfc6a44ee0ab",
+    "url": "/Licenta/index.html"
+  },
+  {
+    "revision": "870d720eacbbd23a8f6c",
+    "url": "/Licenta/static/css/main.acabaae8.chunk.css"
+  },
+  {
+    "revision": "982f2875c230638a674f",
+    "url": "/Licenta/static/js/2.5250584a.chunk.js"
+  },
+  {
+    "revision": "870d720eacbbd23a8f6c",
+    "url": "/Licenta/static/js/main.f77557f0.chunk.js"
+  },
+  {
+    "revision": "ca84f8348699343259c3",
+    "url": "/Licenta/static/js/runtime~main.128e4c22.js"
+  },
+  {
+    "revision": "29bb8ab27c77067124dbc607e830e3be",
+    "url": "/Licenta/static/media/Buchet_info.29bb8ab2.png"
+  },
+  {
+    "revision": "614c25e1cc6d4a86dd6a99264b662932",
+    "url": "/Licenta/static/media/GOLDEN.614c25e1.otf"
+  },
+  {
+    "revision": "25d1846b75ddf24ae760ba83ec4b6fa8",
+    "url": "/Licenta/static/media/blackBox.25d1846b.png"
+  },
+  {
+    "revision": "5fb02731e8767812b8ff0665a027350f",
+    "url": "/Licenta/static/media/blossomF.5fb02731.png"
+  },
+  {
+    "revision": "09e36240bbe1f8c5db29c8a3b37f25b4",
+    "url": "/Licenta/static/media/blueChrysanthemum.09e36240.png"
+  },
+  {
+    "revision": "2a2123ccfdfc1d37893ce3ec0b4d919f",
+    "url": "/Licenta/static/media/buchetreview.2a2123cc.jpg"
+  },
+  {
+    "revision": "26ad95863848042728594e3f86a2a896",
+    "url": "/Licenta/static/media/delivery.26ad9586.png"
+  },
+  {
+    "revision": "ca8446d58eb8315c7a458caf81c6ed86",
+    "url": "/Licenta/static/media/georgia.ca8446d5.ttf"
+  },
+  {
+    "revision": "f83b8ea0a0f43b417437c506a797f019",
+    "url": "/Licenta/static/media/hartie1.f83b8ea0.png"
+  },
+  {
+    "revision": "9a0aafc5d3787dfe5ba58ff870f9cc98",
+    "url": "/Licenta/static/media/hartie2.9a0aafc5.png"
+  },
+  {
+    "revision": "ccc07b88ba266cbdeafc90597e5e4852",
+    "url": "/Licenta/static/media/helpBuchet.ccc07b88.mp4"
+  },
+  {
+    "revision": "0c09c6ae8e1356c4b791deaefa916c29",
+    "url": "/Licenta/static/media/logo.0c09c6ae.png"
+  },
+  {
+    "revision": "3ec81e8ee6bd4fb9f705e2eebc755d9d",
+    "url": "/Licenta/static/media/orangeChrysanthemum.3ec81e8e.png"
+  },
+  {
+    "revision": "6dc74faf5f545202b1e76a4a6de3d363",
+    "url": "/Licenta/static/media/pinkChrysanthemum.6dc74faf.png"
+  },
+  {
+    "revision": "1c6dae9dc5f1a3627e195770d9b7723a",
+    "url": "/Licenta/static/media/redBox.1c6dae9d.png"
+  },
+  {
+    "revision": "25abce773419198dddd76f2544f1155c",
+    "url": "/Licenta/static/media/redLily.25abce77.png"
+  },
+  {
+    "revision": "4ea3f1aa4040cedb8fc7c5a2c39531d4",
+    "url": "/Licenta/static/media/redRose.4ea3f1aa.png"
+  },
+  {
+    "revision": "3cbd5563127effb079e61e45d0876798",
+    "url": "/Licenta/static/media/redTulpin.3cbd5563.png"
+  },
+  {
+    "revision": "adca6c759e2a17514e2f7b5b7d5be9d9",
+    "url": "/Licenta/static/media/violeteTulpin.adca6c75.png"
+  },
+  {
+    "revision": "cef3b3dbd4057b61938ae4565b0acf86",
+    "url": "/Licenta/static/media/whiteBox.cef3b3db.png"
+  },
+  {
+    "revision": "d5dc3c5fb67439cf5e6408dc9c2399f9",
+    "url": "/Licenta/static/media/whiteLily.d5dc3c5f.png"
+  },
+  {
+    "revision": "793cbc7a8324d1ecac4dcd1e640ba4a6",
+    "url": "/Licenta/static/media/whiteRose.793cbc7a.png"
+  },
+  {
+    "revision": "058cf9cd6f20ecb71990dc63b0956c9e",
+    "url": "/Licenta/static/media/yellowLily.058cf9cd.png"
+  },
+  {
+    "revision": "346162bce76743da471c029d9e8b5b77",
+    "url": "/Licenta/static/media/yellowRose.346162bc.png"
+  },
+  {
+    "revision": "360658ef174eddbf1e713ea7156c504f",
+    "url": "/Licenta/static/media/yellowTulpin.360658ef.png"
+  }
+]);
