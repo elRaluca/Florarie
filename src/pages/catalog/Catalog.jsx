333d7f0fd45ffa00ca8139699f9ff045b7d4b973
Gheorghita Raluca <elenaralucalorena@gmail.com>
2024-07-10 17:14:21 +0300

    Update environment variables and rebuild

--- src/pages/catalog/Catalog.jsx
+++ src/pages/catalog/Catalog.jsx
@@ -16,7 +16,7 @@ const Catalog = () => {
     const fetchProducts = async () => {
       try {
         const response = await fetch(
-          "${process.env.REACT_APP_BACKEND_URL}/public/product"
+          `${process.env.REACT_APP_BACKEND_URL}/public/product`
         );
         if (!response.ok) {
           throw new Error("Network response was not ok");
